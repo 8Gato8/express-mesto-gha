5b164312307b4dcd93b4f04c48ddf9d67ab060ce
Sergey Skotcelyas <sergey.sckotcelias@yandex.ru>
2023-05-27 16:10:07 +0800

    fix: Исправить ошибки в названиях в app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-router.use(express.json());
-router.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
 
