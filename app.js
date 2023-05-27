b0588a1b39c2950c5f00460f17e02dd2628a7565
Sergey Skotcelyas <sergey.sckotcelias@yandex.ru>
2023-05-28 05:04:31 +0800

    refactor: Подключить helmet и limiter

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
@@ -8,8 +10,15 @@ const nonexistentPathErrorHandler = require('./middlewares/nonexistentPathErrorH
 const { PORT = 3000 } = process.env;
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(limiter);
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
